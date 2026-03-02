0e7d0b03389020b9054c2ed7e1dd19e176d93b63
Shivansh Mishra <shivnsh01@gmail.com>
2026-03-02 15:15:00 +0530

    Sum of First N Natural Numbers

---
+++ src/SumOfNaturals.java
@@ -0,0 +1,11 @@
+// WAP to get sum of first n natural numbers.
+
+public class SumOfNaturals{
+	public static void main(String[] args){
+		int sum = 0;
+		for ( int i = 1; i <= 10; i++){
+			sum = sum + i;
+		}
+		System.out.println("The sum of First 10 Naturals = " + sum);
+	}
+}
