df9b4e459937b8147f7aeb61bfad3010cb68ffae
Shivansh Mishra <shivnsh01@gmail.com>
2026-03-02 15:06:40 +0530

    Table Generator Code

---
+++ src/TableGenerator.java
@@ -0,0 +1,14 @@
+// WAP to accept a number from user and display table of it.
+
+import java.util.Scanner;
+class TableGenerator{
+	public static void main(String[] args){
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the number: ");
+			int a = sc.nextInt();
+			System.out.println("Table is as follows:");
+			for(int i = 1; i <= 10; i++){
+				System.out.println(a +" x "+ i + " = " + a*i);
+			}
+	}
+}
