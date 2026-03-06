c1699c30aa02a7186e6cb325fee97d999570b5d8
Shivansh Mishra <shivnsh01@gmail.com>
2026-03-06 13:52:09 +0530

    Created a new folder names arrays, and did Demo 1

---
+++ src/arrays/arrDemo1.java
@@ -0,0 +1,15 @@
+// 06-03-2026
+// static array main
+
+class arrDemo1
+{
+	public static void main(String args[])
+	{
+		int i;
+		int marks[] = {78,45,78,45,36,98};
+
+		for(i=0; i<6; i++){
+			System.out.println(marks[i]);
+		}
+	}
+}
